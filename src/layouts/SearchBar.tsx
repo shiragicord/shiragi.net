8edaf19ace2e19ae03b0b1faeb7c4e71a7f9b8fa
shiragi <shiragi@mma.club.uec.ac.jp>
2024-12-16 14:19:36 +0900

    Update

--- src/layouts/SearchBar.tsx
+++ src/layouts/SearchBar.tsx
@@ -95,7 +95,7 @@ export default function SearchBar({ searchList }: Props) {
           <div key={item.slug} className={"col-12 mb-8 sm:col-6"}>
             {item.data.image && (
               <a
-                href={`/${item.slug}`}
+                href={`/~shiragi/${item.slug}`}
                 className="rounded-lg block hover:text-primary overflow-hidden group"
               >
                 <img
@@ -120,7 +120,7 @@ export default function SearchBar({ searchList }: Props) {
                     {item.data.categories.map((category: string, i: number) => (
                       <li key={i} className="inline-block">
                         <a
-                          href={`/categories/${slugify(category)}`}
+                          href={`/~shiragi/categories/${slugify(category)}`}
                           className="mr-2 hover:text-primary font-medium"
                         >
                           {humanize(category)}
@@ -135,7 +135,7 @@ export default function SearchBar({ searchList }: Props) {
 
             <h3 className="mb-2">
               <a
-                href={`/${item.slug}`}
+                href={`/~shiragi/${item.slug}`}
                 className="block hover:text-primary transition duration-300"
               >
                 {item.data.title}
